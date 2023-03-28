9bb9e89885c44d6eec3e0d0d9ec3c08c88f7a7dd
ashrataslima <68125704+ashrataslima@users.noreply.github.com>
2023-03-28 04:47:23 -0700

    added

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
-    <App /></HashRouter>
+    <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
