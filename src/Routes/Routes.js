639b009b03468e9b7f668900a890b8eb45cc17b5
ashrataslima <68125704+ashrataslima@users.noreply.github.com>
2023-03-28 17:47:49 -0700

    added stufff

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import { Contact } from "../Components/Contact/Contact";
 import { HomePage } from "../Components/HomePage/HomePage";
 import { About } from "../Components/About/About";
@@ -9,6 +9,7 @@ import {ProjectLinks} from "../Components/ProjectLinks/ProjecctLinks";
 export const ProjectRoutes = React.memo(() => {
     return (
         <BrowserRouter>
+            <HashRouter basename="hkhan054">
             <Routes>
                 <Route index path="/hkhan054" element={<HomePage />} />
                 <Route index path="/hkhan054/Contact" element={<Contact />} />
@@ -16,6 +17,7 @@ export const ProjectRoutes = React.memo(() => {
                 <Route index path="/hkhan054/Projects" element={<Projects/>} />
                 <Route index path="/hkhan054/KnowledgeHome" element={<ProjectLinks/>} />
             </Routes>
+            </HashRouter>
         </BrowserRouter>
     );
 });
